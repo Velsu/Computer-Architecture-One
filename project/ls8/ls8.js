faceefa57c597d318f0191ca73e8a602a0da47bf
Velsu <riftmage50@gmail.com>
2018-06-12 21:53:28 +0200

    Implemented feature to read instructions from a file

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,5 +1,6 @@
 const RAM = require("./ram");
 const CPU = require("./cpu");
+const fs = require("fs");
 
 /**
  * Load an LS8 program into memory
@@ -7,23 +8,39 @@ const CPU = require("./cpu");
  * TODO: load this from a file on disk instead of having it hardcoded
  */
 function loadMemory() {
+  const argv = process.argv.slice(2);
+  if (argv.length != 1) {
+    console.log("specify name of the file to read instructions from");
+    process.exit(1);
+  }
+
+  const filename = argv[0];
+  const readfile = fs.readFileSync(filename, "utf8");
+  const lines = readfile
+    .trim()
+    .split(/[\r\n]+/g)
+    .filter(line => line[0] != "#");
+
+  const program = [];
+
+  lines.forEach(line => program.push(line.split(" ")[0]));
   // Hardcoded program to print the number 8 on the console
 
-  const program = [
-    // print8.ls8
-    "10011001", // LDI R0,8  Store 8 into R0
-    "00000000",
-    "00001000",
-    "10011001",
-    "00000001",
-    "00001001",
-    "10101010", // MUL R0, R1
-    "00000000",
-    "00000001",
-    "01000011", // PRN R0    Print the value in R0
-    "00000000",
-    "00000001" // HLT       Halt and quit
-  ];
+  // const program = [
+  //   // print8.ls8
+  //   "10011001", // LDI R0,8  Store 8 into R0
+  //   "00000000",
+  //   "00001000",
+  //   "10011001",
+  //   "00000001",
+  //   "00001001",
+  //   "10101010", // MUL R0, R1
+  //   "00000000",
+  //   "00000001",
+  //   "01000011", // PRN R0    Print the value in R0
+  //   "00000000",
+  //   "00000001" // HLT       Halt and quit
+  // ];
 
   // Load the program into the CPU's memory a byte at a time
   for (let i = 0; i < program.length; i++) {
